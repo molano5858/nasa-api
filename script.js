71ccf301e3bf103fae36b1358c56aed8620da1db
molano5858 <molano5858@gmail.com>
2023-10-23 18:42:48 -0500

    Creating fetch request and improve some styles

---
+++ script.js
@@ -0,0 +1,19 @@
+// NASA API
+const count = 10;
+const apiKey = "DEMO_KEY";
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+
+let resultsArray = [];
+
+// get 10 pictures from NASA API
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+    console.log(resultsArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getNasaPictures();
