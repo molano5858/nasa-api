fa00727b8e2663c24b8147d361f19ad504cbec6c
molano5858 <molano5858@gmail.com>
2023-10-24 13:33:39 -0500

    Creating favorites functionality

--- script.js
+++ script.js
@@ -5,15 +5,35 @@ const saveConfirmed = document.querySelector(".save-confirmed");
 const loader = document.querySelector(".loader");
 
 // NASA API
-const count = 10;
+const count = 4;
 const apiKey = "DEMO_KEY";
 const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
 
 let resultsArray = [];
+let favorites = {};
 
-// function to uptdate DOM
-function updateDOM() {
-  resultsArray.forEach((element) => {
+// Add to favorites function
+function saveFavorite(itemUrl) {
+  // loop result array to select favorite
+  resultsArray.forEach((item) => {
+    if (item.url.includes(itemUrl) && !favorites[itemUrl]) {
+      favorites[itemUrl] = item;
+      saveConfirmed.hidden = false;
+      setTimeout(() => {
+        saveConfirmed.hidden = true;
+      }, 2000);
+      localStorage.setItem("nasaFavorites", JSON.stringify(favorites));
+    }
+  });
+}
+
+// create DOMNodes
+function createDOMNodes(page) {
+  const currentArray =
+    page === "favorites" ? Object.values(favorites) : resultsArray;
+  // Object.values because favorites is an object so then like this can use the forEach method
+  console.log("current array", currentArray);
+  currentArray.forEach((element) => {
     // creating card element
     const card = document.createElement("div");
     card.classList.add("card");
@@ -24,7 +44,7 @@ function updateDOM() {
     link.setAttribute("target", "_blank");
     // creating img element
     const image = document.createElement("img");
-    image.setAttribute("src", element.hdurl);
+    image.setAttribute("src", element.url);
     image.setAttribute("alt", element.title);
     image.loading = "lazy";
     image.classList.add("img-card-top");
@@ -35,12 +55,16 @@ function updateDOM() {
     const titleElement = document.createElement("h5");
     titleElement.textContent = element.title;
     titleElement.classList.add("card-title");
+    // Creagint add to favorites
     const addFavorite = document.createElement("p");
     addFavorite.textContent = "Add to Favorite";
     addFavorite.classList.add("clickable2");
+    addFavorite.setAttribute("onclick", `saveFavorite('${element.url}')`);
+    // Creating description
     const cardText = document.createElement("p");
     cardText.textContent = element.explanation;
     cardText.classList.add("card-text");
+    // Footer
     const footerCardText = document.createElement("small");
     footerCardText.classList.add("text-muted");
     const dateElement = document.createElement("strong");
@@ -50,18 +74,22 @@ function updateDOM() {
       ? element.copyright
       : " Copy Right";
     // Append each element
-    card.appendChild(link);
+    cardBody.append(titleElement, addFavorite, cardText, footerCardText);
+    footerCardText.append(dateElement, copyRightCard);
     link.appendChild(image);
-    card.appendChild(cardBody);
-    cardBody.appendChild(titleElement);
-    cardBody.appendChild(addFavorite);
-    cardBody.appendChild(cardText);
-    cardBody.appendChild(footerCardText);
-    footerCardText.appendChild(dateElement);
-    footerCardText.appendChild(copyRightCard);
-
+    card.append(link, cardBody);
     imagesContainer.appendChild(card);
   });
+}
+
+// function to uptdate DOM
+function updateDOM(page) {
+  // get favorites from localStoraga
+  if (localStorage.getItem("nasaFavorites")) {
+    favorites = JSON.parse(localStorage.getItem("nasaFavorites"));
+    console.log(favorites);
+  }
+  createDOMNodes(page);
 }
 
 // get 10 pictures from NASA API
@@ -69,11 +97,15 @@ async function getNasaPictures() {
   try {
     const response = await fetch(apiUrl);
     resultsArray = await response.json();
-    console.log(resultsArray);
-    updateDOM();
+    updateDOM("favorites");
   } catch (error) {
     console.log(error);
   }
 }
 
 getNasaPictures();
+
+// event listeners
+resultsNav.addEventListener("click", () => {
+  console.log(favorites);
+});
